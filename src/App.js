f5e1bd99aceafb2ea9af44170e1b68e5f90062fd
Asi Oren <asioren@gmail.com>
2020-01-04 15:25:37 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/cat-logo.gif'
+import './styles/global.scss';
+
+const tags = ['serious', 'grumpy', 'sleeping', 'white', 'happy', 'old', 'fat', 'cute', 'fail', 'stupid', 'funny', 'gif']
+
+function CatCard(props) {
+  return (
+    <section class="card-cat card">
+    <img class="cat-image" alt="cat" src={"https://cataas.com/cat/" + tags[props.tagNum]}/>
+    <div class="cat-tag">{tags[props.tagNum]}</div>
+  </section> 
+
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header class="main-nav" id="nav">
+        <img alt="logo"src={logo} />
+        <h1>My cat website</h1>
+        <p>We’re all cat lovers. That’s why we’re here. But have you ever stopped to wonder why we find cats so incredible loveable?</p>
+        <p>With Valentine’s Day just around the corner, it seemed the perfect time to explore our fascination with our self-domesticated feline friends.</p>
       </header>
+      <main class="home">
+        {tags.map((value, index) => {
+          return <CatCard tagNum={index} />
+        })}
+      </main>
+      <footer>
+        <small>all rights reserved to cats @2019</small>
+      </footer>
     </div>
   );
 }
