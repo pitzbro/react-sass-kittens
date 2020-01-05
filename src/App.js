e72b197e45a179f5ad984c26f9cc9a79cc4ffb68
pitzbro <asioren@gmail.com>
2020-01-05 14:44:52 +0200

    after lesson

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ const tags = ['serious', 'grumpy', 'sleeping', 'white', 'happy', 'old', 'fat', '
 
 function CatCard(props) {
   return (
-    <section class="card-cat card">
-    <img class="cat-image" alt="cat" src={"https://cataas.com/cat/" + tags[props.tagNum]}/>
-    <div class="cat-tag">{tags[props.tagNum]}</div>
+    <section className="card-cat card square-ratio">
+    <img className="cat-image" alt="cat" src={"https://cataas.com/cat/" + tags[props.tagNum]}/>
+    <div className="cat-tag flex justify-center align-center">{tags[props.tagNum]}</div>
   </section> 
 
   );
@@ -16,20 +16,20 @@ function CatCard(props) {
 
 function App() {
   return (
-    <div className="App">
-      <header class="main-nav" id="nav">
-        <img alt="logo"src={logo} />
+    <div className="app app-container">
+      <header className="main-header container flex column align-center justify-center" id="nav">
+        <img className="logo" alt="logo"src={logo} />
         <h1>My cat website</h1>
         <p>We’re all cat lovers. That’s why we’re here. But have you ever stopped to wonder why we find cats so incredible loveable?</p>
         <p>With Valentine’s Day just around the corner, it seemed the perfect time to explore our fascination with our self-domesticated feline friends.</p>
       </header>
-      <main class="home">
+      <main className="home container cards-container">
         {tags.map((value, index) => {
           return <CatCard tagNum={index} />
         })}
       </main>
       <footer>
-        <small>all rights reserved to cats @2019</small>
+        <small className="container">all rights reserved to cats @2019</small>
       </footer>
     </div>
   );
